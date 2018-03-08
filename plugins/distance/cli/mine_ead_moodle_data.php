8751271397197dd827b8ed86aa9f4b933ae3e400
leolleocomp <leolleo.comp@gmail.com>
2018-03-08 19:25:12 -0300

    CREATE - class constant\query and ADD method create_base to miner class

--- plugins/distance/cli/mine_ead_moodle_data.php
+++ plugins/distance/cli/mine_ead_moodle_data.php
@@ -1,8 +1,9 @@
 <?php
 define('CLI_SCRIPT', true);
-
 require(__DIR__.'/../../../config.php');
 require_once(__DIR__.'/../classes/miner.php');
+require_once(__DIR__.'/../classes/constant/query.php');
+require_once($CFG->libdir.'/clilib.php');
 
 $report = new report_distance_miner();
-echo $report->getQuery();
+cli_writeln($report->create_base('alunos_semestre_adm_publica'));
