e83a73eb71c3de2c074d4fc066069b8914580365
leolleocomp <leolleo.comp@gmail.com>
2018-03-28 14:26:16 -0300

    ADD - course_id model

---
+++ plugins/distance/classes/models/course_id.php
@@ -0,0 +1,12 @@
+<?php
+namespace report_distance\models;
+
+class course_id
+{
+	const table = "id_disciplinas";
+
+	const get = "
+		SELECT DISTINCT(disciplina_id), course_id
+		FROM {".basis::table."}
+		WHERE course_id = ?";
+}
