c0ec8f4a79de1ee853bdf7154650eea6fa05bf0d
leolleocomp <leolleo.comp@gmail.com>
2018-03-31 14:10:03 -0300

    ADD - purge method to discipline model

--- plugins/distance/classes/models/discipline.php
+++ plugins/distance/classes/models/discipline.php
@@ -1,16 +1,18 @@
-<?php 
-namespace report_distance\models; 
+<?php
+namespace report_distance\models;
 
 class discipline
 {
 	const table = "disciplinas";
 
-	const get = " 
-		SELECT DISTINCT (disciplina_id), 
+	const get = "
+		SELECT DISTINCT (disciplina_id),
 			course_id,
-			data_inicio, 
-			data_fim 
-		FROM {".basis::table."} 
+			data_inicio,
+			data_fim
+		FROM {".basis::table."}
 		WHERE course_id = ?
 		ORDER BY disciplina_id";
+
+	const purge = "DELETE FROM {".self::table."}";
 }
