04f30ada3b95827b64bc8266ca67bd16bb6ecb57
leolleocomp <leolleo.comp@gmail.com>
2018-04-18 21:33:06 -0300

    ADD - translation to pt-br

---
+++ plugins/local/distance/lang/pt_br/local_distance.php
@@ -0,0 +1,19 @@
+<?php
+// PLUGIN TEXT
+$string['pluginname'] 			= "Visão Geral da distância transacional";
+$string['distance_desc'] 		= "Plugin que permite acompanhar o envolvimento do aluno com a plataforma moodle";
+
+
+// NAVIGATION TEXT
+$string['distance_navigation_title']		= "Relatório";
+$string['distance_navigation_item']		= "Relatório do Curso";
+$string['distance_navigation_settings_item']	= "Relatório do Curso";
+
+// INDEX PAGE TEXT
+$string['distance_forum_title'] 		= "Variáveis dos alunos relacionadas aos fóruns";
+$string['distance_environment_title'] 		= "Variáveis relacionadas aos acessos dos alunos ao ambiente";
+
+$string['distance_course_input'] 	= "Selecione o curso:";
+$string['distance_student_input'] 	= "Selecione o aluno:";
+
+
