2985cb5b941f14ae642525c56579c34fbf8c7dcd
leolleocomp <leolleo.comp@gmail.com>
2018-04-18 21:32:50 -0300

    ADD - translation to pt-br

--- plugins/local/distance/lang/en/local_distance.php
+++ plugins/local/distance/lang/en/local_distance.php
@@ -1,19 +1,19 @@
 <?php
 // PLUGIN TEXT
-$string['pluginname'] 			= "Transactional distance overview";
-$string['distance_desc'] 		= "Plugin to provide graphs that allow teacher keep up with student behaviour inside Moodle";
+$string['pluginname'] 			= "Visão Geral da distância transacional";
+$string['distance_desc'] 		= "Plugin que permite acompanhar o envolvimento do aluno com a plataforma moodle";
 
 
 // NAVIGATION TEXT
-$string['distance_navigation_title']			= "Report Plugin";
-$string['distance_navigation_item']				= "Course Report";
-$string['distance_navigation_settings_item']	= "Course Report";
+$string['distance_navigation_title']		= "Relatório";
+$string['distance_navigation_item']		= "Relatório do Curso";
+$string['distance_navigation_settings_item']	= "Relatório do Curso";
 
 // INDEX PAGE TEXT
-$string['distance_forum_title'] 		= "Variables related to students access to the environment";
-$string['distance_environment_title'] 	= "Variables related to students access to the environment";
+$string['distance_forum_title'] 		= "Variáveis dos alunos relacionadas aos fóruns";
+$string['distance_environment_title'] 		= "Variáveis relacionadas aos acessos dos alunos ao ambiente";
 
-$string['distance_course_input'] 	= "Select course:";
-$string['distance_student_input'] 	= "Select student:";
+$string['distance_course_input'] 	= "Selecione o curso:";
+$string['distance_student_input'] 	= "Selecione o aluno:";
 
 
