94dad4820a21905149bfaadf5c5b261cc2b5ef17
leolleocomp <leolleo.comp@gmail.com>
2018-05-01 23:33:10 -0300

    UDPATE - transaction distance calculus to occur every day at 4:30 a.m.

--- plugins/local/distance/db/tasks.php
+++ plugins/local/distance/db/tasks.php
@@ -3,8 +3,8 @@ $tasks = [
 	[
 		'classname' => 'local_distance\task\mine_ead_moodle_data',
 		'blocking' => 0,
-		'minute' => '0',
-		'hour' => '0',
+		'minute' => '30',
+		'hour' => '4',
 		'day' => '*',
 		'dayofweek' => '*',
 		'month' => '*'
