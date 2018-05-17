db2360e0b6dc7890eaafa6f2754bde955664aa61
Gustavo Marques <gutodisse@gmail.com>
2018-05-17 12:02:57 -0300

    Correcao dos titulos dos graficos

--- plugins/local/distance/lang/en/local_distance.php
+++ plugins/local/distance/lang/en/local_distance.php
@@ -10,11 +10,12 @@ $string['distance_navigation_item']		= "Gráficos do Curso";
 $string['distance_navigation_settings_item']	= "Gráficos do Curso";
 
 // INDEX PAGE TEXT
-$string['distance_forum_title'] 		= "Indicadores dos alunos relacionados aos fóruns";
-$string['distance_environment_title'] 		= "Indicadores relacionados aos acessos dos alunos ao ambiente";
+$string['distance_forum_title'] 		= "Indicadores de Fóruns e Mensagens";
+$string['distance_environment_title'] 		= "Indicadores de acessos";
 
 $string['distance_course_input'] 	= "Selecione o curso:";
 $string['distance_student_input'] 	= "Selecione o aluno:";
 $string['distance_group_input'] 	= "Selecione o pólo:";
 
-$string['task_desc'] = "";
+// TASK NAME
+$string['task_desc'] = "minera os dados da distancia transacional";
