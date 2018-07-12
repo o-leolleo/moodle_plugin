d0788c11867649ddcde6741dc210cf424ff3086d
leolleocomp <leolleo.comp@gmail.com>
2018-07-11 21:08:40 -0300

    RM - import of job miner

--- plugins/local/distance/classes/task/mine_ead_moodle_data.php
+++ plugins/local/distance/classes/task/mine_ead_moodle_data.php
@@ -1,10 +1,8 @@
-<?php 
+<?php
 namespace local_distance\task;
 
 ini_set('max_exection_time', 6000);
 ini_set('memory_limit', '512M');
-
-use local_distance\job\miner;
 
 class mine_ead_moodle_data extends \core\task\scheduled_task
 {
